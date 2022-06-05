ed31a5d0e2963b1adf741c5132b48cad16fadbaa
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2022-06-05 16:11:28 +0800

    Add redirection

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
 
 import React from 'react';
-import { Link, Routes, Route } from 'react-router-dom';
+import { Link, Navigate, Routes, Route, useLocation } from 'react-router-dom';
 
 import './styles/app.scss';
 import Home from './components/Home';
 import Contact from './components/Contact';
 
-class App extends React.Component {
-  render () {
-    return (
-      <>
-        <header>
-          <Link to={ process.env.PUBLIC_URL }><img src="/images/logo.png" alt="Website logo" width={32} height={32} /></Link>
-          <Link to="/">Home</Link>
-          <Link to="/contact">Contact</Link>
-        </header>
+function App () {
+  const location = useLocation();
+  const search = new URLSearchParams(location.search);
+  const redirect = search.get('redirect');
+  if (redirect !== null) return <Navigate to={redirect} />;
 
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
+  return (
+    <>
+      <header>
+        <Link to={ process.env.PUBLIC_URL }><img src="/images/logo.png" alt="Website logo" width={32} height={32} /></Link>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+      </header>
 
-        <footer>
-          Copyright &copy; 2022 <a href={ process.env.PUBLIC_URL }>Adriane Justine Tan</a>
-        </footer>
-      </>
-    );
-  }
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+
+      <footer>
+        Copyright &copy; 2022 <a href={ process.env.PUBLIC_URL }>Adriane Justine Tan</a>
+      </footer>
+    </>
+  );
 }
 
 export default App;
