ad6f80f570e56934be99b5eb2751baa64d817783
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2022-06-08 17:05:47 +0800

    Remove unnecessary redirection

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,48 @@
 
 import React from 'react';
-import { Link, Navigate, Routes, Route, useLocation } from 'react-router-dom';
+import { Link, Routes, Route } from 'react-router-dom';
 
 import './styles/app.scss';
 import Home from './components/Home';
 import Contact from './components/Contact';
 import NotFound from './components/NotFound';
 
-function App () {
-  const location = useLocation();
-  const search = new URLSearchParams(location.search);
-  const redirect = search.get('redirect');
-  if (redirect !== null) return <Navigate to={redirect} />;
+class App extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <header>
+          <Link to={ process.env.PUBLIC_URL }><img src="/images/logo.png" alt="Website logo" width={32} height={32} /></Link>
+          <Link to="/">Home</Link>
+          <Link to="/contact">Contact</Link>
+        </header>
 
-  return (
-    <React.Fragment>
-      <header>
-        <Link to={ process.env.PUBLIC_URL }><img src="/images/logo.png" alt="Website logo" width={32} height={32} /></Link>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contact</Link>
-      </header>
+        <main>
+          <Routes>
+            <Route path="/">
+              <Route index element={<Home />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </main>
 
-      <main>
-        <Routes>
-          <Route path="/">
-            <Route index element={<Home />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </main>
+        <footer>
+          <div className="mb-2">
+            Copyright &copy; 2022 <a href={ process.env.PUBLIC_URL }>Adriane Justine Tan</a>
+          </div>
 
-      <footer>
-        <div className="mb-2">
-          Copyright &copy; 2022 <a href={ process.env.PUBLIC_URL }>Adriane Justine Tan</a>
-        </div>
+          <div className="cc-license">
+            <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/" className="d-block">
+              <img src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" alt="Creative Commons License" />
+            </a>
 
-        <div className="cc-license">
-          <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/" className="d-block">
-            <img src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" alt="Creative Commons License" />
-          </a>
-
-          This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
-        </div>
-      </footer>
-    </React.Fragment>
-  );
+            This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+          </div>
+        </footer>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/PageNotFound.jsx
+++ src/PageNotFound.jsx
@@ -4,34 +4,36 @@ import React from 'react';
 import './styles/app.scss';
 import NotFound from './components/NotFound';
 
-function PageNotFound () {
-  return (
-    <React.Fragment>
-      <header>
-        <a href={ process.env.PUBLIC_URL }><img src="/images/logo.png" alt="Website logo" width={32} height={32} /></a>
-        <a href="/">Home</a>
-        <a href="/contact">Contact</a>
-      </header>
+class PageNotFound extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <header>
+          <a href={ process.env.PUBLIC_URL }><img src="/images/logo.png" alt="Website logo" width={32} height={32} /></a>
+          <a href="/">Home</a>
+          <a href="/contact">Contact</a>
+        </header>
 
-      <main>
-        <NotFound />
-      </main>
+        <main>
+          <NotFound />
+        </main>
 
-      <footer>
-        <div className="mb-2">
-          Copyright &copy; 2022 <a href={ process.env.PUBLIC_URL }>Adriane Justine Tan</a>
-        </div>
+        <footer>
+          <div className="mb-2">
+            Copyright &copy; 2022 <a href={ process.env.PUBLIC_URL }>Adriane Justine Tan</a>
+          </div>
 
-        <div className="cc-license">
-          <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/" className="d-block">
-            <img src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" alt="Creative Commons License" />
-          </a>
+          <div className="cc-license">
+            <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/" className="d-block">
+              <img src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" alt="Creative Commons License" />
+            </a>
 
-          This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
-        </div>
-      </footer>
-    </React.Fragment>
-  );
+            This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+          </div>
+        </footer>
+      </React.Fragment>
+    );
+  }
 }
 
 export default PageNotFound;
