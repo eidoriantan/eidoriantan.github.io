ab0c11c4a6abd7bfb666e54dcf38b409c06f4857
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2022-06-08 20:18:22 +0800

    Use `mb-2` class instead

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -57,22 +57,22 @@ class ContactForm extends React.Component {
         </div>
 
         <form action="https://formspree.io/f/xwkyygaw" method="post" onSubmit={this.handleSubmit}>
-          <div className="form-group">
+          <div className="form-group mb-2">
             <label htmlFor="name">Name:</label>
             <input type="text" id="name" name="name" placeholder="Your name" required />
           </div>
 
-          <div className="form-group">
+          <div className="form-group mb-2">
             <label htmlFor="email">Email:</label>
             <input type="email" id="email" name="email" placeholder="name@example.com" required />
           </div>
 
-          <div className="form-group">
+          <div className="form-group mb-2">
             <label htmlFor="subject">Subject:</label>
             <input type="text" id="subject" name="subject" placeholder="Subject" required />
           </div>
 
-          <div className="form-group">
+          <div className="form-group mb-2">
             <label htmlFor="message">Message:</label>
             <textarea id="message" name="message" rows="5" placeholder="Your message" required></textarea>
           </div>
