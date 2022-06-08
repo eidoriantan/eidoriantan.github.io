6c80976b59f7df6caa04325275f24918c15463a4
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2022-06-08 14:22:33 +0800

    Add CC BY-SA license

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App () {
   if (redirect !== null) return <Navigate to={redirect} />;
 
   return (
-    <>
+    <React.Fragment>
       <header>
         <Link to={ process.env.PUBLIC_URL }><img src="/images/logo.png" alt="Website logo" width={32} height={32} /></Link>
         <Link to="/">Home</Link>
@@ -32,9 +32,19 @@ function App () {
       </main>
 
       <footer>
-        Copyright &copy; 2022 <a href={ process.env.PUBLIC_URL }>Adriane Justine Tan</a>
+        <div className="mb-2">
+          Copyright &copy; 2022 <a href={ process.env.PUBLIC_URL }>Adriane Justine Tan</a>
+        </div>
+
+        <div className="cc-license">
+          <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/" className="d-block">
+            <img src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" alt="Creative Commons License" />
+          </a>
+
+          This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+        </div>
       </footer>
-    </>
+    </React.Fragment>
   );
 }
 
--- src/PageNotFound.jsx
+++ src/PageNotFound.jsx
@@ -6,7 +6,7 @@ import NotFound from './components/NotFound';
 
 function PageNotFound () {
   return (
-    <>
+    <React.Fragment>
       <header>
         <a href={ process.env.PUBLIC_URL }><img src="/images/logo.png" alt="Website logo" width={32} height={32} /></a>
         <a href="/">Home</a>
@@ -18,9 +18,19 @@ function PageNotFound () {
       </main>
 
       <footer>
-        Copyright &copy; 2022 <a href={ process.env.PUBLIC_URL }>Adriane Justine Tan</a>
+        <div className="mb-2">
+          Copyright &copy; 2022 <a href={ process.env.PUBLIC_URL }>Adriane Justine Tan</a>
+        </div>
+
+        <div className="cc-license">
+          <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/" className="d-block">
+            <img src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" alt="Creative Commons License" />
+          </a>
+
+          This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+        </div>
       </footer>
-    </>
+    </React.Fragment>
   );
 }
 
